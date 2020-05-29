bb64b8a9c081e419a19d232d1b029dffd1f05da0
yangjam-cn <1171267147@qq.com>
2020-05-29 21:49:17 +0800

    c++

---
+++ cpp/hello.cpp
@@ -0,0 +1,15 @@
+/*
+ * @Author: yangjam
+ * @Date: 2020-05-29 21:46:49
+ * @LastEditTime: 2020-05-29 21:48:20
+ * @Environment: win10, gcc 8.1.0 & python 3.7.3
+ * @Description: test git and c++
+ */ 
+#include<iostream>
+int main(void)
+{
+    using namespace std;
+    cout << "hello c++" << endl;
+    cout << "hello git" << endl;
+    return 0;
+}
