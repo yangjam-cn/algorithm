6daa9f4f1005ec661e9512639891e0d72269d99c
yangjam-cn <1171267147@qq.com>
2020-08-04 16:20:17 +0800

    增加基准函数的注释

--- c/quick_sort.c
+++ c/quick_sort.c
@@ -6,7 +6,7 @@
  * @Version: demo
  * @Date: 2020-07-19 12:49:05
  * @History: 修改历史列表，每条修改记录应包括修改日期、修改者及修改内容简述
- * @LastEditTime: 2020-07-19 13:38:22
+ * @LastEditTime: 2020-08-04 16:16:27
  */ 
 #include <stdio.h>
 #include <time.h>
@@ -32,18 +32,25 @@ int partition(int *arr, int left, int right)
     int temp = arr[left];
     while (left < right)
     {
+        /*初始left指向基准值，从后往前寻找比基准值小的值，将其前移*/
         while ((left < right) && (temp < arr[right]))
         {
             right--;
-        }
+        }    /*end while(temp>=arr[right])*/
+        /*退出while后，right指向比temp小的值*/
+        /*用right覆盖temp的值，此时right即指向temp应该在的位置*/
+        /*实质是交换位置，不过每次都交换temp，故只需要用需要调整位置的值覆盖temp，游标指向temp，省略了对temp的操作*/
         arr[left] = arr[right];
+
+        /*从前往后遍历寻找比temp大的值，将其后移*/
         while ((left < right) && (temp >= arr[left]))
         {
             left++;
-        }
+        }    /*end while(temp<arr[left])*/
         arr[right] = arr[left];
-    }
-    arr[right] = temp;
+        /*此时left指向temp*/
+    }    /*end while(right==left)*/
+    arr[right] = temp;    /*完成交换操作的最后一步*/
     return right;
 }
 
